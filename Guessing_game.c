cf428bdeacc28598459dec721ad367cd7308d3f0
Anushikha-Rani <ranianushikha@gmail.com>
2024-05-21 19:20:56 +0530

    Guessing_game

---
+++ Guessing_game.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int secret_number;
+  int guess_number;
+
+  secret_number = 5;
+
+  int i;
+  int guess_limit;
+
+  guess_limit = 3;
+
+  for (i = 1; i <= guess_limit; i++)
+  {
+    printf("Guess Number %d")
+  }
+}
